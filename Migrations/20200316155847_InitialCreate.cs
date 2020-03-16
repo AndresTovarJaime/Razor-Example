ccbe5589d0cfdbd590f32c39f0e5fdadad40e9bd
Andres Tovar Jaime <andres-tovar-jaime@hotmail.com>
2020-03-16 11:36:20 -0500

    Scaffolded Class

---
+++ Migrations/20200316155847_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Razor_Example.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    IndependenceDay = table.Column<DateTime>(nullable: false),
+                    Population = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
